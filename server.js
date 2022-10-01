98cf4b3132ed478d1f0ebe367f5a6dd7d245142c
fudongdong <fudongdonguestc@gmail.com>
2022-10-01 14:31:29 +0800

    style

--- server.js
+++ server.js
@@ -23,29 +23,36 @@ app.get('/placeholder', function(req, res) {
 app.get('/*', function(req, res) {
   let args = parseurl(req).pathname.replace('/placeholder/', '').split('+')
   const text = req.query.text;
-  const content = req.query.text || `${width}x${height}`
+
+  const textColor = req.query.color || '#aaa';
+  const bgColor = req.query.bgColor || '#eee';
+  const borderColor = req.query.borderColor || 'rgba(0,0,0,.1)'
+  const crossColor = req.query.crossColor || '#ddd';
+
   let size = args[0].split('x')
   let width = size[0]
   let height = size[1]
+  const content = req.query.text || `${width}x${height}`
+
   let font_size = Math.round(Math.max(12, Math.min(Math.min(width, height) * 0.75, 0.75 * Math.max(width, height) / 12)))
   let rectStyle = ''
   let elements = ''
   if (args.length > 1) {
     if (args.indexOf('border') > 0) {
-      rectStyle += 'stroke-width: 3px; stroke: rgba(0,0,0,.1);'
+      rectStyle += `stroke-width: 3px; stroke: ${borderColor};`
     }
     if (args.indexOf('cross') > 0) {
       elements += `
-        <line xmlns="http://www.w3.org/2000/svg" x1="0" y1="0" x2="${width}" y2="${height}" stroke-width="1" stroke="#ddd" />
-        <line xmlns="http://www.w3.org/2000/svg" x1="${width}" y1="0" x2="0" y2="${height}" stroke-width="1" stroke="#ddd" />
+        <line xmlns="http://www.w3.org/2000/svg" x1="0" y1="0" x2="${width}" y2="${height}" stroke-width="1" stroke="${crossColor}" />
+        <line xmlns="http://www.w3.org/2000/svg" x1="${width}" y1="0" x2="0" y2="${height}" stroke-width="1" stroke="${crossColor}" />
       `
     }
   }
   res.writeHead(200, {'Content-Type': 'image/svg+xml'})
   res.end(`<svg xmlns="http://www.w3.org/2000/svg" width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" preserveAspectRatio="none">
-    <rect width="${width}" height="${height}" style="${rectStyle}" fill="#eee"/>
+    <rect width="${width}" height="${height}" style="${rectStyle}" fill="${bgColor}"/>
     ${elements}
-    <text text-anchor="middle" x="${width/2}" y="${height/2}" style="fill:#aaa;font-weight:bold;font-size:${font_size}px;font-family:Arial,Helvetica,sans-serif;dominant-baseline:central">
+    <text text-anchor="middle" x="${width/2}" y="${height/2}" style="fill:${textColor};font-weight:bold;font-size:${font_size}px;font-family:Arial,Helvetica,sans-serif;dominant-baseline:central">
     ${content}
     </text>
   </svg>`)
